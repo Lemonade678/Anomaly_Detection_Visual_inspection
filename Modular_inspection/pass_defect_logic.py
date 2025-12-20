ce11e368eccfbec0126b3d2981a187b7d25686cd
LemonadeC._Th <101620059+Lemonade678@users.noreply.github.com>
2025-12-20 23:07:30 +0700

    Add initial anomaly detection logic in pass_defect_logic.py

---
+++ Modular_inspection/pass_defect_logic.py
@@ -0,0 +1,11 @@
+import cv2
+import np
+#wip program
+anomaly_detected = False
+gate = False
+#insert anomaly detected
+if Anomaly_detected = True
+   gate = True
+
+if ___name__ == ""main"":
+      pass
